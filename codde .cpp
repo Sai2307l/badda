6c937e52f589b34b4a58e06d6d7cd971c7b9172f
B.Sai Bhaskar <saibhamidipati2004@gmail.com>
2026-04-08 19:30:07 +0530

    asd

--- codde .cpp
+++ codde .cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     cout << "Hello, World!\n";
     cout << "Welcome to C++ programming.\n";
     cout << "git commit has been done.\n";
